dd3e57243d2e42758be81e3759585bdea489de3c
Yadeta Getachew <86324546+yadetagetachew@users.noreply.github.com>
2022-12-13 16:20:18 -0800

    doc:react app created

---
+++ src/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+// const TodoList = () => {
+//   return (
+//     <div>
+//       hello world
+//     </div>
+//   )
+// }
+// export default TodoList
+
+
+export default function TodoList() {
+  return (
+    <div>
+      hello world
+    </div>
+  )
+}
